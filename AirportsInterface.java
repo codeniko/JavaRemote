657138b6cab9906b3114e499d0116403c0905ab0
Nikolay Feldman <nf139@cs.rutgers.edu>
2014-11-09 20:12:25 -0500

    working

---
+++ AirportsInterface.java
@@ -0,0 +1,8 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+import PlaceData.PlaceDataProto.*;
+
+public interface AirportsInterface extends Remote {
+	public RemoteResult lookup(double lat, double lon) throws RemoteException;
+}
---
+++ PlacesInterface.java
@@ -0,0 +1,8 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+import PlaceData.PlaceDataProto.*;
+
+public interface PlacesInterface extends Remote {
+	public RemoteResult lookup(String city, String state) throws RemoteException;
+}
